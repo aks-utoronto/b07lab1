0e2e843a6b091b3ab2bf908dd5cfccbce8b6cb0a
aks-456 <37085635+aks-456@users.noreply.github.com>
2022-10-04 23:46:14 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,19 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.File;
+
+
+
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
+    public static void main(String [] args) throws Exception {
+        //Polynomial p = new Polynomial();
+       // System.out.println(p.evaluate(3));
         double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
+        int [] p1 = {2, 1, 3, 4};
+        Polynomial poly1 = new Polynomial(c1, p1);
         double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
+        int [] p2 = {7, 4, 2, 2, 0};
+        Polynomial poly2 = new Polynomial(c2, p2);
+        Polynomial s = poly1.add(poly2);
+        Polynomial q = poly1.multiply(poly2);
+
+
+        File in_file = new File("text.txt");
+        Polynomial p_file = new Polynomial(in_file);
         
-        
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+        File in_file2 = new File("text2.txt");
+        Polynomial p_file_2 = new Polynomial(in_file2);
+
+        Polynomial result = p_file_2.multiply(p_file);
+        result.saveToFile("text3.txt");
+
+        //System.out.println("s(0.1) = " + q.evaluate(0.1));
+
+        for(int i = 0; i < p_file.powers.length; i++) {
+            System.out.print(p_file.coefficients[i]);
+            System.out.print(" ");
+            System.out.print( p_file.powers[i]);
+            System.out.println(" ");
+        }
+        // System.out.println();        
+        // // if(s.hasRoot(1))
+        //     System.out.println("1 is a root of s");
+        // else
+        //     System.out.println("1 is not a root of s");
+
+
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,43 +1,183 @@
+// import File;
+// import java.io.start;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.File;
+
+
 public class Polynomial {
 	
 	double [] coefficients; // change size
+	int [] powers;
 
-	public Polynomial() {
-		coefficients = new double[1];
-		coefficients[0] = 0;
-	}	
 
-	public Polynomial(double []array_parameter) {
-		coefficients = array_parameter;
+	public Polynomial(double []array_parameter, int []power_array) {
+		coefficients = new double[array_parameter.length];
+		for(int i = 0; i < array_parameter.length; i++) {
+			coefficients[i] = array_parameter[i];
+		}
+
+		powers = new int[power_array.length];
+		for(int i = 0; i < power_array.length; i++) {
+			powers[i] = power_array[i];
+		}
 	}
 
+	public Polynomial(File input_file) throws Exception {
+		BufferedReader input = new BufferedReader(new FileReader(input_file));
+		String str = input.readLine();
+		int count = 0;
+		String [] count_arr = str.split("");
+		
+		String [] str_arr = str.split("[\\+]");
+		double []coefficients_f = new double[count_arr.length];
+		int []powers_f = new int[count_arr.length];
+		int c_counter = 0;
+		int p_counter = 0;
+
+		for(int i = 0; i < str_arr.length; i++) {
+			
+			String [] str_arr2 = str_arr[i].split("\\-");
+
+			for(int j = 0; j < str_arr2.length; j++) {
+				if(str_arr2[j].indexOf("x") == -1) {
+					if(j > 0) {
+						coefficients_f[c_counter] = Double.parseDouble("-" + str_arr2[j]);
+						c_counter++;
+						powers_f[p_counter] = 0;
+						p_counter++;
+					} else {
+						coefficients_f[c_counter] = Double.parseDouble(str_arr2[j]);
+						c_counter++;
+						powers_f[p_counter] = 0;
+						p_counter++;
+					}
+				} else {
+					String [] str_arr3 = str_arr2[j].split("");
+					if(j > 0) {
+						coefficients_f[c_counter] = Double.parseDouble("-" + str_arr3[0]);
+						c_counter++;
+						powers_f[(p_counter)] = Integer.parseInt(str_arr3[2]);
+						p_counter++;
+					} else {
+						coefficients_f[c_counter] = Double.parseDouble(str_arr3[0]);
+						c_counter++;
+						powers_f[(p_counter)] = Integer.parseInt(str_arr3[2]);
+						p_counter++;
+					}
+				}
+			}
+		}
+
+
+		coefficients = new double[p_counter];
+		powers = new int[p_counter];
+
+		for(int i  = 0; i < p_counter; i++) {
+			coefficients[i] = coefficients_f[i];
+			powers[i] = powers_f[i];
+		}
+
+	}
+
+
 	public Polynomial add(Polynomial poly) {
+
+		int max_length = powers.length + poly.powers.length;
 		
-		int arr_length = Math.max(poly.coefficients.length, this.coefficients.length);
+		int []p1_powers = new int[max_length]; // list of powers for new polynomial
+		double []p1_coeff = new double[max_length];
 
-		double [] coeff_add = new double[arr_length];
-		coeff_add[arr_length-1] = 0;
-
-		for(int i = 0; i < this.coefficients.length; i++) {
-			coeff_add[i] += this.coefficients[i];
+		int coeff_index = 0;
+		for(int i = 0; i < powers.length; i++) {
+			p1_powers[i] = powers[i];
+			p1_coeff[i] = coefficients[i];
+			coeff_index = i;
 		}
 
+		coeff_index += 1;
+		int counter = 0;
+		boolean is_repeat = false;
+		for(int i = 0; i < poly.powers.length; i++) {
+			is_repeat = false;
+			for(int j = 0; j < powers.length; j++) {
+				if(powers[j] == poly.powers[i]) {
+					if((p1_coeff[j] + poly.coefficients[i]) != 0.0) {
+						p1_coeff[j] += poly.coefficients[i];
+					}
 
-		for(int i = 0; i < poly.coefficients.length; i++) {
-			coeff_add[i] += poly.coefficients[i];
+					is_repeat = true;
+				}
+			}
+			//Make a new entry
+			if(!is_repeat) {
+				if((poly.coefficients[i]) != 0.0) {
+					p1_powers[coeff_index+counter] = poly.powers[i];
+					p1_coeff[coeff_index+counter] = poly.coefficients[i];
+					counter++;
+				}
+			}
+			
 		}
 
-		Polynomial poly1 = new Polynomial(coeff_add);
+		int []f_powers = new int[coeff_index + counter];
+		double []f_coeff = new double[coeff_index + counter];
+		//Trim arrays
+		for(int i = 0; i < coeff_index + counter; i++) {
+			f_powers[i] = p1_powers[i];
+			f_coeff[i] = p1_coeff[i];
+		}
+
+		Polynomial p1 = new Polynomial(f_coeff, f_powers);
+
+		return p1;
 		
-		return poly1;
+	}
+
+
+	public Polynomial multiply(Polynomial poly) {
+
+		Polynomial [] poly_arr = new Polynomial[poly.powers.length * powers.length];
+
+		int counter = 0;
+		for(int i = 0; i < powers.length; i++) {
+			for(int j = 0; j < poly.powers.length; j++) {
+				double [] p1_coeff = {coefficients[i] * poly.coefficients[j]};
+				int [] p1_powers = {powers[i] + poly.powers[j]};
+
+				poly_arr[counter] = new Polynomial(p1_coeff, p1_powers);
+				counter++;
+		
+
+			}
+		}
+
+		int index = 0;
+		Polynomial new_poly = poly_arr[index];
+		index++;
+		while(poly_arr[index].coefficients[0] == 0.0) {
+			new_poly = poly_arr[index];
+			index++;
+		}
+
+		//Polynomial new_poly = poly_arr[0];
+		for(int i = index; i < poly_arr.length; i++) {
+			if(poly_arr[i].coefficients[0] != 0.0)
+				new_poly = new_poly.add(poly_arr[i]);
+		}
+
+		return new_poly;
+
 	}
 
 	public double evaluate(double x) {
 		double result = 0;
 		for(int i = 0; i < coefficients.length; i++) {
-			result += (coefficients[i])*Math.pow(x, i);
+			result += (coefficients[i])*Math.pow(x, powers[i]);
 		}
-
+    
 		return result;
 	}
 
@@ -47,6 +187,32 @@ public class Polynomial {
 			return true;
 		}
 		return false;
+	}
+
+
+
+	public void saveToFile(String path) throws Exception {
+		FileWriter writer = new FileWriter(path);  
+		BufferedWriter buffer = new BufferedWriter(writer);
+		String output = "";
+		for(int i = 0; i < coefficients.length; i++) {
+			int coeff_int = (int)coefficients[i];
+			String coeff_output = "";
+			if(coeff_int > 0 && i != 0) {
+				coeff_output += "+" + Integer.toString(coeff_int);
+			} else {
+				coeff_output += Integer.toString(coeff_int);
+			}
+
+			if(powers[i] != 0) {
+				output += coeff_output + "x" + Integer.toString(powers[i]);
+			} else {
+				output += coeff_output;
+			}
+			
+		}  
+		buffer.write(output);  
+		buffer.close();  
 	}
 
 
